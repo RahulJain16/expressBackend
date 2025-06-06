fdd7856e7e7f570da6ab433842d446ccfb300dbf
Rahuljain16 <rj267043@gmail.com>
2025-06-06 20:46:35 +0530

    latest commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
     next();
 });
 
-// Authentication middleware (for testing)
+// Authentication midvdfgedfgdleware (for testing)
 app.use('/api', (req, res, next) => {
     console.log('API route accessed');
     next();
